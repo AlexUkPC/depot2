13d9bc6a8f62db8c4bd3c55bc6ee17e3939e178c
AlexUkPC <alexrogna@yahoo.com>
2021-04-01 11:08:23 +0100

    changed to WSL

---
+++ depot2/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+  resources :products
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "\letter_opener"
+  end
+end
